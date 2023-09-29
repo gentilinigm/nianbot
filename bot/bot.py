9d0d1f7423bef08e5f5e881e86a5a0ae90c0ae2a
gian <38076337+gentilinigian@users.noreply.github.com>
2023-09-29 15:51:59 +0200

    Load extension and sync commands at bot startup

--- bot/bot.py
+++ bot/bot.py
@@ -1,8 +1,16 @@
+import asyncio
+import logging
+import types
 from contextlib import suppress
 
 import aiohttp
-import psycopg2
+import discord
 from discord.ext import commands
+from psycopg2.extensions import connection
+
+from bot import exts
+from bot.utils.extensions import walk_extensions
+from bot.utils.scheduling import create_task
 
 
 class StartupError(Exception):
@@ -14,14 +22,14 @@ class StartupError(Exception):
 
 
 class Bot(commands.Bot):
-    """A subclass that implements some features."""
+    """A bot subclass that implements some features."""
 
     def __init__(
             self,
             *args,
             guild_id: int,
             http_session: aiohttp.ClientSession,
-            db_connection: psycopg2,
+            db_connection: connection,
             **kwargs
     ):
         """
@@ -29,15 +37,83 @@ class Bot(commands.Bot):
 
         Args:
             guild_id: The ID of the KyoStinV guild.
+            http_session: The aiohttp.ClientSession()
+            db_connection: The active Postgresql connection
         """
         super().__init__(
             *args,
             **kwargs
         )
 
+        self.guild_id = guild_id
         self.http_session = http_session
         self.db_connection = db_connection
-        self.guild_id = guild_id
+
+        self._guild_available: asyncio.Event | None = None
+        self._extension_loading_task: asyncio.Task | None = None
+
+    async def setup_hook(self) -> None:
+        """Default async initialisation method for discord.py."""
+        await super().setup_hook()
+
+        self._guild_available = asyncio.Event()
+
+        await self.load_extensions(exts)
+
+    async def _load_extensions(self, module: types.ModuleType) -> None:
+        """Load all the extensions within the given module and save them to ``self.all_extensions``."""
+        logging.info("Waiting for guild %d to be available before loading extensions.", self.guild_id)
+
+        await self.wait_until_guild_available()
+        logging.info("Loading extensions...")
+        self.all_extensions = walk_extensions(module)
+
+        for extension in self.all_extensions:
+            create_task(self.load_extension(extension))
+
+    async def _sync_app_commands(self) -> None:
+        """Sync global & guild specific application commands after extensions are loaded."""
+        await self._extension_loading_task
+        await self.tree.sync()
+        await self.tree.sync(guild=discord.Object(self.guild_id))
+
+    async def load_extensions(self, module: types.ModuleType, sync_app_commands: bool = True) -> None:
+        """Load all the extensions within the given ``module`` and save them to ``self.all_extensions``."""
+        self._extension_loading_task = create_task(self._load_extensions(module))
+        if sync_app_commands:
+            create_task(self._sync_app_commands())
+
+    async def on_guild_available(self, guild: discord.Guild) -> None:
+        """
+        Set the internal guild available event when self.guild_id becomes available.
+
+        If the cache appears to still be empty (no members, no channels, or no roles), the event
+        will not be set and `guild_available_but_cache_empty` event will be emitted.
+        """
+        if guild.id != self.guild_id:
+            return
+
+        if not guild.roles or not guild.members or not guild.channels:
+            logging.error("Guild available event was dispatched but the cache appears to still be empty!")
+            return
+
+        self._guild_available.set()
+
+    async def on_guild_unavailable(self, guild: discord.Guild) -> None:
+        """Clear the internal guild available event when self.guild_id becomes unavailable."""
+        if guild.id != self.guild_id:
+            return
+
+        self._guild_available.clear()
+
+    async def wait_until_guild_available(self) -> None:
+        """
+        Wait until the guild that matches the ``guild_id`` given at init is available (and the cache is ready).
+
+        The on_ready event is inadequate because it only waits 2 seconds for a GUILD_CREATE
+        gateway event before giving up and thus not populating the cache for unavailable guilds.
+        """
+        await self._guild_available.wait()
 
     async def close(self) -> None:
         """Closes connections (Discord, aiohttp session, etc)."""
---
+++ bot/utils/error_handling.py
@@ -0,0 +1,57 @@
+import logging
+
+from discord import Forbidden, Message
+
+"""
+MIT License.
+
+Copyright (c) 2021 Python Discord
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+async def handle_forbidden_from_block(error: Forbidden, message: Message | None = None) -> None:
+    """
+    Handles ``discord.Forbidden`` 90001 errors, or re-raises if ``error`` isn't a 90001 error.
+
+    Args:
+        error: The raised ``discord.Forbidden`` to check.
+        message: The message to reply to and include in logs, if error is 90001 and message is provided.
+    """
+    if error.code != 90001:
+        # The error ISN'T caused by the bot attempting to add a reaction
+        # to a message whose author has blocked the bot, so re-raise it
+        raise error
+
+    if not message:
+        logging.info("Failed to add reaction(s) to a message since the message author has blocked the bot")
+        return
+
+    if message:
+        logging.info(
+            "Failed to add reaction(s) to message %d-%d since the message author (%d) has blocked the bot",
+            message.channel.id,
+            message.id,
+            message.author.id,
+        )
+        await message.channel.send(
+            f":x: {message.author.mention} failed to add reaction(s) to your message as you've blocked me.",
+            delete_after=30
+        )
